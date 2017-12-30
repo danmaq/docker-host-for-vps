e957746b07a44a7c4c614302dcc4d4e1d2c3ff20
Shuhei Nomura <info@danmaq.com>
2017-12-30 12:32:00 +0900

    Refactored.

--- script/kujirax/Setup.js
+++ script/kujirax/Setup.js
@@ -1,60 +1,17 @@
 'use strict';
 
 import appRoot from 'app-root-path';
-import { spawn } from 'child_process';
 import Docker from 'dockerode';
 import fs from 'fs';
 import path from 'path';
 import tar from 'tar';
 
+import { AnsibleHosts, Dockerfile } from './Template';
+
 import './type';
 
 const { kujirax, author } =
 require(path.join(appRoot.path, 'package.json'));
-
-/**
- * Create body of Dockerfile.
- * @param {string} image Image name on which to base temporary container for deployment.
- * @returns {string} body of Dockerfile.
- */
-const Dockerfile =
-    image =>
-    `
-FROM ${image}
-LABEL authors="Shuhei Nomura <info@danmaq.com>"
-
-ADD script.tar.gz /root/
-ADD caravan.tar.gz /root/
-ADD kujirax.json /root/
-ADD hosts /root/
-RUN cd /root && chmod 755 start.sh
-CMD ["/root/start.sh"]
-`
-    .trim();
-
-/**
- * Create body of hosts for Ansible.
- * @param {string} host
- * @param {string} port
- * @param {string} user
- * @param {string} key
- * @returns {string} body of hosts for Ansible.
- */
-const AnsibleHosts =
-    (host, port, user, pass, key) =>
-    (keySetting =>
-        `
-[kujirax]
-${host}
-
-[all:vars]
-ansible_port=${port}
-ansible_user=${user}
-ansible_ssh_pass=${pass}
-ansible_become_pass=${pass}
-${keySetting}
-`
-    )(key ? `ansible_private_key_file=/root/${key}` : '').trim();
 
 /** Root directory of temporary container for setup. */
 const setupHome = path.join(appRoot.path, '.setup');
@@ -93,10 +50,9 @@ const preDeployAsync =
  * @param {string} image Docker image.
  */
 const createDockerfileAsync =
-    async(image) => {
-        const text = Dockerfile(image);
-        fs.writeFileSync(path.join(setupHome, 'Dockerfile'), text);
-    };
+    async(image) =>
+    fs.writeFileSync(
+        path.join(setupHome, 'Dockerfile'), Dockerfile(image));
 
 /**
  *
@@ -104,11 +60,9 @@ const createDockerfileAsync =
  * @param {Caravan.SSH} ssh
  */
 const createAnsibleHostsFileAsync =
-    async(host, ssh) => {
-        const { user, port, pass, key } = ssh;
-        const text = AnsibleHosts(host, port, user, pass, key);
-        fs.writeFileSync(path.join(setupHome, 'hosts'), text);
-    };
+    async(host, ssh) =>
+    fs.writeFileSync(
+        path.join(setupHome, 'hosts'), AnsibleHosts(host, ssh));
 
 /**
  * Create docker instance.
@@ -145,14 +99,13 @@ const createImageAsync =
  * Run a Docker image for the temporary container for deployment.
  * @param {string} image Docker image.
  * @param {string} name
- * @param {string} shell
  */
 const runImageAsync =
-    async(image, name, shell) => {
+    async(image, name) => {
         const container =
             await createDocker().run(
                 image,
-                shell,
+                undefined,
                 process.stdout, { name }, {});
         await container.remove();
     };
@@ -162,7 +115,7 @@ const runImageAsync =
  */
 const setupAsync =
     async(caravan) => {
-        const { host, ssh, setup: { image, tempImage, name, shell } } =
+        const { host, ssh, setup: { image, tempImage, name } } =
         caravan;
         console.info('Correct items...');
         await preDeployAsync(caravan);
@@ -174,7 +127,7 @@ const setupAsync =
         // However, It is more correct to wait until the Docker image is retrieved.
         await new Promise(r => setTimeout(r, 3000));
         console.info('Run setup image...');
-        await runImageAsync(tempImage, name, shell);
+        await runImageAsync(tempImage, name);
     }
 
 export default setupAsync;
